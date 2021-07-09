eb6a96c1c2b1905df2e3e73da6c0689bb3ee6a27
名前 <ec2-user@ip-172-31-19-185.us-east-2.compute.internal>
2021-07-09 03:17:43 +0000

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,35 @@
+Rails.application.routes.draw do
+  get 'topics/new'
+  get 'sessions/new'
+  get 'users/new'
+  get 'pages/index'
+  root 'pages#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+Rails.application.routes.draw do
+  get 'topics/new'
+  get 'sessions/new'
+  root 'pages#index'
+  get 'pages/help'
+
+  resources :users
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+end
+
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  root 'pages#index'
+  get 'pages/help'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  resources :topics
+end
