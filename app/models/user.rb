119a97eeefe4a58b6c54ec64482d7895cc8309d4
名前 <ec2-user@ip-172-31-19-185.us-east-2.compute.internal>
2021-07-12 02:49:46 +0000

    commit2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   validates :email, presence: true
   
   has_secure_password
+  
+  has_many :favorite_topics, through: :favorites, source: 'topic'
+  has_many :favorites
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,4 @@
 Rails.application.routes.draw do
-  get 'topics/new'
-  get 'sessions/new'
-  get 'users/new'
-  get 'pages/index'
-  root 'pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-
-Rails.application.routes.draw do
-  get 'topics/new'
-  get 'sessions/new'
-  root 'pages#index'
-  get 'pages/help'
-
-  resources :users
-  
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-end
-
-Rails.application.routes.draw do
   get 'sessions/new'
 
   root 'pages#index'
@@ -32,4 +10,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :topics
+  
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
 end
